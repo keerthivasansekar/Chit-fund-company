0261956bcdcbb6e28beacf5bfac67cbf53302687
Keerthivasan Sekar <s.keerthivasan@kfinsolutions.com>
2016-09-16 21:20:47 +0530

    Updated files

--- PHP & CSS/db/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'core.inc.php';
-session_destroy();
-$refer = 'index.php';
-header('Location: '.$refer);
-
-?>
--- PHP & CSS/db/redirect.php
+++
@@ -1,6 +0,0 @@
-<?php
- require 'core.inc.php';
- $grp_id = $_POST['grp_id'];
- $_SESSION['grp_id']=$grp_id;
- header('Location: addauction2.php');
-?>
